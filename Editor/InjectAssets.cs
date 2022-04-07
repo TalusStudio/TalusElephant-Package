a1734094fcf0d361569199da851136148e92ae3b
Emre Kovanci <kovanciemre@gmail.com>
2022-04-07 19:18:32 +0300

    Revert "update inject"
    This reverts commit e86178ad694244ea359c5387856869cedc11a967.

--- Editor/InjectAssets.cs
+++ Editor/InjectAssets.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using Editor;
-
 using UnityEditor;
 using UnityEngine;
 
@@ -10,8 +7,6 @@ namespace ElephantSDK
 {
     public class InjectAssets
     {
-        public const string TALUS_BACKEND_KEYWORD = "ENABLE_BACKEND";
-
         public const string ScenePath = "Packages/com.talus.taluselephant/";
         public const string AssetPath = "Packages/com.talus.taluselephant/UI/Textures/Resources/";
 
@@ -40,24 +35,6 @@ namespace ElephantSDK
                     Path.Combine(elephantScenePath, "elephant_scene.unity"));
 
                 Debug.Log("elephant_scene copied to: " + elephantScenePath);
-
-                if (!DefineSymbols.Contains(TALUS_BACKEND_KEYWORD))
-                {
-                    DefineSymbols.Add(TALUS_BACKEND_KEYWORD);
-                    Debug.Log("Activating Talus-Elephant-Backend!..");
-                }
-
-                // add elephant scene to the active scenes.
-                var editorBuildSettingsScenes = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
-                var elephantScene = new EditorBuildSettingsScene(elephantScenePath, true);
-
-                if (!editorBuildSettingsScenes.Contains(elephantScene))
-                {
-                    editorBuildSettingsScenes.Insert(0, elephantScene);
-                    EditorBuildSettings.scenes = editorBuildSettingsScenes.ToArray();
-
-                    Debug.Log("elephant_scene registered as entry point in build settings!..");
-                }
             }
             catch (Exception e)
             {
